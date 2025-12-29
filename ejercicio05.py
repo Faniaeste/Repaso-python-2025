cd4c4e89b789fa06d297a8225ee29dc3dd2d9f5f
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2025-12-29 20:16:05 +0100

    Le agregamos poder elejir que operación quieres hacer, y con un while que lo recoge todo para hacerlo una y otra vez hasta que lo paremos

--- ejercicio05.py
+++ ejercicio05.py
@@ -13,31 +13,57 @@ Retos
 Controla que las entradas sean números de forma que el programa no avance si no se introduce un número
 No permitas introducir números negativos
 """
+seguir = True
+while seguir:
 
+    #Entrada
+    numero1 = input("ingrese el primer número: ")
+    numero2 = input("ingrese el segundo número: ")
+    operacion = input("Ingrese la operación: +, -, * , /")
 
-#Entrada
-numero1 = input("ingrese el primer número: ")
-numero2 = input("ingrese el segundo número: ")
-
-#control
-"""
-#Primera prueba y no nos sale, hasta que descubrimos isnumeric.
-while float(numero1) < 0 or float(numero2) < 0:
+    #control
+    """
+    #Primera prueba y no nos sale, hasta que descubrimos isnumeric.
+    while float(numero1) < 0 or float(numero2) < 0:
     print("No puedes ingresar numeros negativos, porfavor vuelve a introducirlos")
     numero1 = input("ingrese el primer número: ")
     numero2 = input("ingrese el segundo número: ")
-"""
-while not(numero1.isnumeric()) or not (numero2.isnumeric) :
-    print("No puedes ingresar numeros negativos, porfavor vuelve a introducirlos")
-    numero1 = input("ingrese el primer número: ")
-    numero2 = input("ingrese el segundo número: ")
-#Tranformación
-numero1 = float(numero1)
-numero2 = float(numero2)
-suma = numero1 + numero2
-resta = numero1 - numero2
-producto = numero1 * numero2
-division = numero1 / numero2
+    """
 
-#Salida
-print(f"La suma es: {suma}\nLa resta es: {resta}\nEl producto es: {producto}\nLa division es:  {division}")
+
+    while operacion not in "+, -, * , /":
+        print("Debe ingresar operacion valida, porfavor vuelva a ingresar la operación")
+        operacion = input("Ingrese la operación: +, -, * , /")
+
+    while not(numero1.isnumeric()) or not (numero2.isnumeric) :
+        print("No puedes ingresar numeros negativos, porfavor vuelve a introducirlos")
+        numero1 = input("ingrese el primer número: ")
+        numero2 = input("ingrese el segundo número: ")
+
+
+    #Tranformación
+    numero1 = float(numero1)
+    numero2 = float(numero2)
+    suma = numero1 + numero2
+    resta = numero1 - numero2
+    producto = numero1 * numero2
+    division = numero1 / numero2
+
+    #Salida~
+    resultado = 0
+    if operacion == '+':
+        resultado = numero1 + numero2
+    elif operacion == '-':
+        resultado = numero1 - numero2
+    elif operacion == '*':
+     resultado = numero1 * numero2
+    elif operacion == '/':
+        resultado = numero1 / numero2
+    print(f"{numero1} {operacion} {numero2} = {resultado}")
+
+    continuar = input("Desea seguir utilizando la calculadora? s/n")
+    if continuar == "n":
+        seguir = False
+        print("Fin de la operación")
+        #break
+        #print(f"La suma es: {suma}\nLa resta es: {resta}\nEl producto es: {producto}\nLa division es:  {division}")
