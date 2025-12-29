da60e5282761f962a67b965c9fe0e020ae83a5ff
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2025-12-29 19:45:12 +0100

    Ejercicio completo

---
+++ ejercicio05.py
@@ -0,0 +1,43 @@
+"https://colab.research.google.com/drive/1NdN6ID3rMZvi3GiT8P34ctn7XdqDpssi"
+
+"""
+Operaciones aritméticas
+Escribe un programa que pida dos números y que devuelva su suma, resta, producto y división.
+
+Restricciones
+Convertir las cadenas de entrada en números
+Separar convenientemente la entrada, transformación de cadena en números y salida separados
+Crea una única sentencia de salida con los saltos de línea adecuados (sólo un print)
+
+Retos
+Controla que las entradas sean números de forma que el programa no avance si no se introduce un número
+No permitas introducir números negativos
+"""
+
+
+#Entrada
+numero1 = input("ingrese el primer número: ")
+numero2 = input("ingrese el segundo número: ")
+
+#control
+"""
+#Primera prueba y no nos sale, hasta que descubrimos isnumeric.
+while float(numero1) < 0 or float(numero2) < 0:
+    print("No puedes ingresar numeros negativos, porfavor vuelve a introducirlos")
+    numero1 = input("ingrese el primer número: ")
+    numero2 = input("ingrese el segundo número: ")
+"""
+while not(numero1.isnumeric()) or not (numero2.isnumeric) :
+    print("No puedes ingresar numeros negativos, porfavor vuelve a introducirlos")
+    numero1 = input("ingrese el primer número: ")
+    numero2 = input("ingrese el segundo número: ")
+#Tranformación
+numero1 = float(numero1)
+numero2 = float(numero2)
+suma = numero1 + numero2
+resta = numero1 - numero2
+producto = numero1 * numero2
+division = numero1 / numero2
+
+#Salida
+print(f"La suma es: {suma}\nLa resta es: {resta}\nEl producto es: {producto}\nLa division es:  {division}")
